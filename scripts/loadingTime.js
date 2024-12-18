1aa931c57d9662372d4dd77225e10aa6c077d446
Gushchenko Aleksandr <alexgu525@mail.ru>
2024-12-18 15:59:58 +0300

    fix: make lab7

---
+++ scripts/loadingTime.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+    swal({
+        title: 'Page loaded!',
+        text: `Loading took ${performance.now().toFixed(2)}ms`,
+        icon: 'images/socialcredit.jpeg'
+    });
+})
